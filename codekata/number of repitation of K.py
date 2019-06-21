ba2afd481dcec94eb0a5a8bf493a11bd0f712f2f
ArunkumarJ11 <51688392+ArunkumarJ11@users.noreply.github.com>
2019-06-21 14:35:12 +0530

    Update number of repitation of K.py

--- codekata/number of repitation of K.py
+++ codekata/number of repitation of K.py
@@ -1,8 +1,8 @@
-=map(int,input().split())
-yy=list(map(int,input().split()))
+a1,a2=map(int,input().split())
+xx=list(map(int,input().split()))
 count=0
-if(len(yy)==x1):
- for i in yy:
-  if(i==x2):
+if(len(xx)==a1):
+ for i in xx:
+  if(i==a2):
    count+=1
  print(count) 
